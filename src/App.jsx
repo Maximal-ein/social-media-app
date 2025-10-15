b11ba7eaec6281164578f9ae8e2cf02d1263e5dd
Maxim Kirichenko <3gffd@list.ru>
2025-10-15 12:44:10 +0300

    cleaned

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {useMemo, useState} from 'react';
+import {useState} from 'react';
 import './styles/App.css';
 import PostList from "./components/PostList.jsx";
 import PostForm from "./components/PostForm.jsx";
