f6c26e9f1c23711947efc8b57da096773911f8eb
Maxim Kirichenko <3gffd@list.ru>
2025-10-17 13:42:14 +0300

    fixed delayed switching between pages without useEffect hook

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
     const [page, setPage] = useState(1)
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
     const pagesArray = usePagination(totalPages)
-    const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+    const [fetchPosts, isPostsLoading, postError] = useFetching(async (limit,page) => {
         const response = await PostService.getAll(limit, page)
         setPosts(response.data)
         const totalCount = response.headers['x-total-count']
@@ -30,8 +30,8 @@ function App() {
     })
 
     useEffect(() => {
-        fetchPosts()
-    }, [page]);
+        fetchPosts(limit,page)
+    }, []);
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
@@ -44,6 +44,7 @@ function App() {
 
     const changePage = (page) => {
         setPage(page)
+        fetchPosts(limit,page)
     }
 
     return (
