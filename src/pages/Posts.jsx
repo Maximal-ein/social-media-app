1fb75dc8f542bfd3f65da382adbce1fbe3d2f44d
Maxim Kirichenko <3gffd@list.ru>
2025-10-20 12:51:35 +0300

    Added routing and page About

---
+++ src/pages/Posts.jsx
@@ -0,0 +1,75 @@
+import {useEffect, useState} from 'react';
+import PostList from "../components/PostList.jsx";
+import PostForm from "../components/PostForm.jsx";
+import PostFilter from "../components/PostFilter.jsx";
+import MyModal from "../components/UI/modal/MyModal.jsx";
+import MyButton from "../components/UI/button/MyButton.jsx";
+import {usePosts} from "../hooks/usePosts.js";
+import PostService from "../API/PostService.js";
+import Loader from "../components/UI/Loader/Loader.jsx";
+import {useFetching} from "../hooks/useFetching.js";
+import {getPagesCount} from "../utils/pages.js";
+import Pagination from "../components/UI/pagination/Pagination.jsx";
+
+function Posts() {
+
+    const [posts, setPosts] = useState([])
+    const [filter, setFilter] = useState({sort: '', query: ''})
+    const[modal, setModal] = useState(false)
+    const [totalPages, setTotalPages] = useState(0)
+    const [limit, setLimit] = useState(10)
+    const [page, setPage] = useState(1)
+    const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
+    const [fetchPosts, isPostsLoading, postError] = useFetching(async (limit,page) => {
+        const response = await PostService.getAll(limit, page)
+        setPosts(response.data)
+        const totalCount = response.headers['x-total-count']
+        setTotalPages(getPagesCount(totalCount, limit))
+    })
+
+    useEffect(() => {
+        fetchPosts(limit,page)
+    }, []);
+
+    const createPost = (newPost) => {
+        setPosts([...posts, newPost])
+        setModal(false)
+    }
+
+    const removePost = (post) => {
+        setPosts(posts.filter(p => p.id !== post.id))
+    }
+
+    const changePage = (page) => {
+        setPage(page)
+        fetchPosts(limit,page)
+    }
+
+    return (
+        <div className="App">
+            <MyButton style={{marginTop: 30}}  onClick={() => setModal(true)}>
+                Create post
+            </MyButton>
+            <MyModal visible={modal} setVisible={setModal}>
+                <PostForm create={createPost}/>
+            </MyModal>
+            <hr style={{margin: '15px 0'}}/>
+            <PostFilter filter={filter}
+                        setFilter={setFilter}
+            />
+            {postError &&
+                <h1>Error: {postError}</h1>
+            }
+            {isPostsLoading
+                ? <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}><Loader/></div>
+                : <PostList posts={sortedAndSearchedPosts} title="List of posts" remove={removePost}/>
+            }
+            <Pagination page={page}
+                        changePage={changePage}
+                        totalPages={totalPages}
+            />
+        </div>
+    )
+}
+
+export default Posts
