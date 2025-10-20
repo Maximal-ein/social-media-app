c7ba607243e8a78fa94d3209323d4954d01b044d
Maxim Kirichenko <3gffd@list.ru>
2025-10-20 18:16:49 +0300

    added comments section on post page

--- src/pages/PostIdPage.jsx
+++ src/pages/PostIdPage.jsx
@@ -7,13 +7,19 @@ import Loader from "../components/UI/Loader/Loader.jsx";
 const PostIdPage = () => {
     const params = useParams()
     const [post, setPost] = useState({})
+    const [comments, setComments] = useState([])
     const [fetchPostById, isLoading, error] = useFetching(async (id) => {
         const response = await PostService.getById(id)
         setPost(response.data)
     })
+    const [fetchComments, isComLoading, comError] = useFetching(async (id) => {
+        const response = await PostService.getCommentsByPostId(id)
+        setComments(response.data)
+    })
 
     useEffect(() => {
         fetchPostById(params.id)
+        fetchComments(params.id)
     }, [])
 
     return (
@@ -25,6 +31,23 @@ const PostIdPage = () => {
             {isLoading
                 ? <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}><Loader/></div>
                 : <div>{post.id}. {post.title}</div>
+            }
+            <h2>
+                Comments
+            </h2>
+            {comError &&
+                <h2>Error: {error}</h2>
+            }
+            {isComLoading
+                ? <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}><Loader/></div>
+                : <div>
+                    {comments.map((comm) =>
+                        <div style={{marginTop: 15}}>
+                            <h5>{comm.email}</h5>
+                            <div>{comm.body}</div>
+                        </div>
+                    )}
+                </div>
             }
         </div>
     );
